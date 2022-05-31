dfd02681abee5bcd71e8a1a2c58bf0dcec41baf9
JaGaffney <jagaffney661@msn.com>
2022-05-31 14:54:07 +1000

    feature: added tooltips to bank items

--- src/components/game/player/BankItem.tsx
+++ src/components/game/player/BankItem.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import ReactTooltip from 'react-tooltip';
+
+
+
 export const BankItem = (props) => {
     return (
-        <button className="bank__items-singleItem" key={props.key} onClick={() => {
-            props.bankItemSelectedHandler(props.itemData)
-            props.setActiveItemID(props.id)
-        }}>
-            <img className="bank__items-image" src={props.icon} />
-            <span className="bank__items-name">{props.name && props.name}</span>
-            <span className="bank__items-qty"><span className="bank__items-qty-inner">{props.qty && props.qty}</span></span>
-        </button>
+        props.qty > 0 &&
+        <>
+            <button className="bank__items-singleItem"
+                key={props.key}
+                onClick={() => {
+                    props.bankItemSelectedHandler(props.itemData)
+                    props.setActiveItemID(props.id)
+                }}
+                data-tip={props.name && props.name}
+            >
+                <img className="bank__items-image" src={props.icon} />
+                <span className="bank__items-qty">{props.qty && props.qty}</span>
+            </button>
+            <ReactTooltip className="react__tooltips-override" type="dark" effect="solid" />
+        </>
     )
 }
 

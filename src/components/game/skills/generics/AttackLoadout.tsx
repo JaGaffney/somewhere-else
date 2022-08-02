48249196611168b3ee128b5a2e6e3c5182e03fdb
JaGaffney <jagaffney661@msn.com>
2022-08-02 13:15:24 +1000

    style: added better styling to attacks page

--- src/components/game/skills/generics/AttackLoadout.tsx
+++ src/components/game/skills/generics/AttackLoadout.tsx
@@ -36,8 +36,6 @@ export const AttackLoadout = (props) => {
         if (!isNaN(attackDataID)) {
             props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).changeAttackLocation(parseInt(attackDataID), slot)
         }
-
-
     }
     const passiveDrop = (e, slot: number) => {
         e.preventDefault()
@@ -57,83 +55,88 @@ export const AttackLoadout = (props) => {
         <div className="attackloadout">
 
             <div className="attackloadout__equipped">
-                {props.playerData &&
-                    <div className="attackloadout__equipped-attacks">
-                        {Object.keys(props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).equippedAttacks).map((id, k) => {
-                            const attackID: number = props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).equippedAttacks[id]
-                            const attackData = props.attackData.getAttackById(attackID)
-                            const slot: number = k + 1
-                            return (
-                                <div className="attackloadout__equipped-slot"
-                                    data-tip={attackData && attackData.name}
-                                    key={k}
-                                    onClick={(e) => {
-                                        props.onSelectedPassiveHandler(null)
-                                        if (e.detail === 1) {
-                                            props.onSelectedSkillHandler(attackID)
-                                        }
-                                        if (e.detail === 2) {
-                                            props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).changeAttackLocation(parseInt(null), slot)
-                                            props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).removeAttackFromRotation(slot)
-                                            props.onSelectedSkillHandler(null)
-                                        }
-                                    }}
-                                >
-                                    <div className="attacks__button attacks__button-general" style={{ borderColor: getBackgroundColor(attackData ? attackData.type : "default") }}
-                                        onDragOver={dragOver}
-                                        onDragEnter={dragEnter}
-                                        onDragLeave={dragLeave}
-                                        onDrop={(e) => attackDrop(e, slot)}
+                <div className="attackloadout__equipped-active">
+                    <h3>Active Skills</h3>
+                    {props.playerData &&
+                        <div className="attackloadout__equipped-attacks">
+                            {Object.keys(props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).equippedAttacks).map((id, k) => {
+                                const attackID: number = props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).equippedAttacks[id]
+                                const attackData = props.attackData.getAttackById(attackID)
+                                const slot: number = k + 1
+                                return (
+                                    <div className="attackloadout__equipped-slot"
+                                        data-tip={attackData && attackData.name}
+                                        key={k}
+                                        onClick={(e) => {
+                                            props.onSelectedPassiveHandler(null)
+                                            if (e.detail === 1) {
+                                                props.onSelectedSkillHandler(attackID)
+                                            }
+                                            if (e.detail === 2) {
+                                                props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).changeAttackLocation(parseInt(null), slot)
+                                                props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).removeAttackFromRotation(slot)
+                                                props.onSelectedSkillHandler(null)
+                                            }
+                                        }}
                                     >
-                                        {attackData && <img className="attacks__button-icon" src={attackData && attackData.icon} />}
+                                        <div className="attacks__button attacks__button-general" style={{ borderColor: getBackgroundColor(attackData ? attackData.type : "default") }}
+                                            onDragOver={dragOver}
+                                            onDragEnter={dragEnter}
+                                            onDragLeave={dragLeave}
+                                            onDrop={(e) => attackDrop(e, slot)}
+                                        >
+                                            {attackData ? <img className="attacks__button-icon" src={attackData && attackData.icon} /> : <span className="attacks__button-icon attacks__button-icon-placeholder">{k + 1}</span>}
 
-                                        {attackData &&
-                                            <div className="attacks__button-stats">
-                                                <span className="attacks__button-stats-topLeft">{attackData.cooldown}</span>
-                                                <span className="attacks__button-stats-topRight">{attackData.stamina}</span>
-                                                <span className="attacks__button-stats-bottomRight">{attackData.maxDamage}</span>
-                                            </div>
-                                        }
+                                            {attackData &&
+                                                <div className="attacks__button-stats">
+                                                    <span className="attacks__button-stats-topLeft">{attackData.cooldown}</span>
+                                                    <span className="attacks__button-stats-topRight">{attackData.stamina}</span>
+                                                    <span className="attacks__button-stats-bottomRight">{attackData.maxDamage}</span>
+                                                </div>
+                                            }
+                                        </div>
                                     </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                }
+                                )
+                            })}
+                        </div>
+                    }
+                </div>
 
-                <div className="attackloadout__equipped-talents">
-                    <div className="attackloadout__equipped-slot">
+                <div className="attackloadout__equipped-active">
+                    <h3>Active Passives</h3>
+                    <div className="attackloadout__equipped-attacks">
+
                         {Object.keys(props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).equippedPassives).map((i, k) => {
                             const data = props.playerData.loadout.getLoadoutByNumber(props.playerData.loadout.activeLoadout).getEquippedPassiveAtLocation(i)
                             const slot: number = k + 1
                             return (
-                                <div
-                                    onDragOver={dragOver}
-                                    onDragEnter={dragEnter}
-                                    onDragLeave={dragLeave}
-                                    onDrop={(e) => passiveDrop(e, slot)}
-                                    className="attacks__button"
-                                    key={k}
-                                    data-tip={data && props.passiveData.getPassiveById(data) && data && props.passiveData.getPassiveById(data).name}
-                                    style={{ borderColor: getBackgroundColor(data && props.passiveData.getPassiveById(data) && data && props.passiveData.getPassiveById(data).job) }}
-                                    onClick={(e) => {
-                                        props.onSelectedSkillHandler(null)
-                                        if (e.detail === 1) {
-                                            props.onSelectedPassiveHandler(props.passiveData.getPassiveById(data))
-                                        }
-                                        if (e.detail === 2) {
-                                            props.playerData.passives.changeEquippedPassives(slot, null)
-                                            props.onSelectedPassiveHandler(null)
-                                        }
-                                    }}
-                                >
-                                    <img className="attacks__button-icon" src={data && props.passiveData.getPassiveById(data).icon} />
+                                <div className="attackloadout__equipped-slot">
+                                    <div
+                                        onDragOver={dragOver}
+                                        onDragEnter={dragEnter}
+                                        onDragLeave={dragLeave}
+                                        onDrop={(e) => passiveDrop(e, slot)}
+                                        className="attacks__button"
+                                        key={k}
+                                        data-tip={data && props.passiveData.getPassiveById(data) && data && props.passiveData.getPassiveById(data).name}
+                                        style={{ borderColor: getBackgroundColor(data && props.passiveData.getPassiveById(data) && data && props.passiveData.getPassiveById(data).job) }}
+                                        onClick={(e) => {
+                                            props.onSelectedSkillHandler(null)
+                                            if (e.detail === 1) {
+                                                props.onSelectedPassiveHandler(props.passiveData.getPassiveById(data))
+                                            }
+                                            if (e.detail === 2) {
+                                                props.playerData.passives.changeEquippedPassives(slot, null)
+                                                props.onSelectedPassiveHandler(null)
+                                            }
+                                        }}
+                                    >
+                                        {data ? <img className="attacks__button-icon" src={data && props.passiveData.getPassiveById(data).icon} /> : <span className="attacks__button-icon attacks__button-icon-placeholder">{k + 1}</span>}
+                                    </div>
                                 </div>
                             )
-
-
-
                         })}
+
 
                     </div>
                 </div>
@@ -142,7 +145,7 @@ export const AttackLoadout = (props) => {
 
 
             <ReactTooltip className="react__tooltips-override" type="dark" effect="solid" />
-        </div>
+        </div >
     )
 }
 

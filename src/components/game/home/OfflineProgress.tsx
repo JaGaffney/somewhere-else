c63b0626eea7b037f2121f4d6dd18fa2dafef5bf
JaGaffney <jagaffney661@msn.com>
2022-08-24 17:07:35 +1000

    feature: added tracking of how much gold is made during offline progress

--- src/components/game/home/OfflineProgress.tsx
+++ src/components/game/home/OfflineProgress.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { intToString } from "../../utils/generic"
+
+// @ts-ignore
+import COINS from "../../../images/sidepanel/coins.svg"
+
 export const OfflineProgress = (props) => {
+    // TODO: get this working
     return (
-        <div className="equipment__container-equipment">
+        <div className="equipment__container-equipment offlineProgression">
             <h3>Heres what your settlement has produced since you have been away</h3>
 
-            <span>GP {props.playerData.offline.coins}</span>
-            <span>Salary -{props.playerData.offline.salary}</span>
+            <div className="offlineProgression-gp">
+                <span><img src={COINS} alt="income" />{props.playerData.offline && intToString(props.playerData.offline.coins)}</span>
+                <span className="offlineProgression-gp-negative"><img src={COINS} alt="salary" />-{props.playerData.offline && intToString(props.playerData.offline.salary)}</span>
+                <span className={`${props.playerData.offline && props.playerData.offline.coins - props.playerData.offline.salary > 0 ? "" : "offlineProgression-gp-negative"}`}>
+                    <img src={COINS} alt="total" />{props.playerData.offline && intToString(props.playerData.offline.coins - props.playerData.offline.salary)}
+                </span>
+            </div>
 
-            OfflineProgress</div>
+            OfflineProgress
+        </div>
     )
 }
 

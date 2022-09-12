3fe23788f9feb8986bf8cca3f452c56090b1a36c
JaGaffney <jagaffney661@msn.com>
2022-09-12 17:00:10 +1000

    feature: added more map data

--- src/components/data/seed/icons/mapSeedIcon.ts
+++ src/components/data/seed/icons/mapSeedIcon.ts
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import VILLAGE from "../../../../images/areas/village.png"
 import ENCHANTEDPLANES from "../../../../images/areas/enchantedplanes.png"
+import GRAVEYARD from "../../../../images/areas/graveyard.png"
 
 export const area1 = VILLAGE
-export const area2 = ENCHANTEDPLANES
+export const area2 = GRAVEYARD
+export const area3 = ENCHANTEDPLANES
--- src/components/game/combat/Combat.tsx
+++ src/components/game/combat/Combat.tsx
@@ -54,7 +54,7 @@ export const Combat = (props) => {
         <div className="game__normal">
             <div className="combat__info">
                 <button onClick={() => dispatch({ type: types.CAT_COMBAT })}><img src={COMBAT} /><span>Battle</span></button>
-                <button onClick={() => dispatch({ type: types.CAT_MAP })}><img src={GLOBE} /><span>Map</span></button>
+                <button onClick={() => dispatch({ type: types.CAT_MAP })}><img src={GLOBE} /><span>Areas</span></button>
                 <button><img src={COMPASS} /> <span>Adventure</span></button>
             </div>
             <Popup open={displayDrops} closeOnDocumentClick onClose={closeModal} position="right center" modal>
